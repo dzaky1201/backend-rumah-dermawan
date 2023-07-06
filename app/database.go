8bf1f103056d733912af0c0d500631c8bf38df03
dzaky1201 <dzakyhdr@gmail.com>
2023-07-06 21:40:36 +0700

    connect to heroku

--- app/database.go
+++ app/database.go
@@ -1,13 +1,20 @@
 package app
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 func NewDb() *gorm.DB {
-	dsn := "host=localhost user=postgres password=postgres dbname=rumah_dermawan port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", dbHost, dbUser, dbPass, dbName, dbPort)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
