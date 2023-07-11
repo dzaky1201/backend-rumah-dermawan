64808867181c60f15d2a00fed5eacab3359fd81f
Dzaky Haidar <56240977+dzaky1201@users.noreply.github.com>
2023-07-11 22:48:34 +0700

    Update database.go

--- app/database.go
+++ app/database.go
@@ -7,20 +7,20 @@ import (
 )
 
 func NewDb() *gorm.DB {
-	// load env variables
-	//err := godotenv.Load()
-	//if err != nil {
-	//	log.Fatal("Error loading .env file")
-	//}
-	//dbUser := os.Getenv("DB_USER")
-	//dbPass := os.Getenv("DB_PASSWORD")
-	//dbHost := os.Getenv("DB_HOST")
-	//dbName := os.Getenv("DB_NAME")
-	//dbPort := os.Getenv("DB_PORT")
-	//dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", dbHost, dbUser, dbPass, dbName, dbPort)
+	load env variables
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Jakarta", dbHost, dbUser, dbPass, dbName, dbPort)
 
-	dsn := "host=localhost user=postgres password=postgres dbname=rumah_dermawan port=5432 sslmode=disable TimeZone=Asia/Jakarta"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// dsn := "host=localhost user=postgres password=postgres dbname=rumah_dermawan port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
