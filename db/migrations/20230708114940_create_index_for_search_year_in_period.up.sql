2f864390866a007e2779b931cb1dbd7df6ca714c
dzaky1201 <dzakyhdr@gmail.com>
2023-07-12 13:59:23 +0700

    change migration schema

--- db/migrations/20230708114940_create_index_for_search_year_in_period.up.sql
+++
@@ -1,1 +0,0 @@
-create index year_search_index on year_periods using gin (to_tsvector('indonesian', info_period ->> 'Month'));
---
+++ db/migrations/20230712060243_create_invest_activity_table.up.sql
@@ -0,0 +1,17 @@
+create table invests_activities
+(
+    id               serial      not null,
+    date_note        varchar(50) not null,
+    description      varchar(50) not null,
+    amount           money         not null,
+    type_transaction TRANSACTION_CATEGORY,
+    year_period_id   int,
+    created_at       timestamp default current_timestamp,
+    primary key (id),
+    constraint fk_year_period_invests foreign key (year_period_id) references year_periods (id)
+
+);
+
+create index desc_invest_search_index on invests_activities using gin (to_tsvector('indonesian', description));
+create index type_transaction_invests_search_index on invests_activities using gin (to_tsvector('indonesian', type_transaction));
+create index date_note_invest_search_index on invests_activities using gin (to_tsvector('indonesian', date_note));
